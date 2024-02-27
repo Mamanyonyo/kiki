f75c7e49abcd1e6d5326d7a58001ff1175b9b347
Widenyo <93363813+Widenyo@users.noreply.github.com>
2024-02-26 22:19:35 -0300

    nigger

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
-const MAX_SPEED = 200
+const MAX_SPEED = 125
+const ACCELERATION_SMOOTHING = 25
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,14 +9,14 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	var movement_vector = get_movement_vector()
 	var direction = movement_vector.normalized()
-	velocity = direction * MAX_SPEED
+	var target_velocity = direction * MAX_SPEED
+	velocity = velocity.lerp(target_velocity, 1 - exp(-delta * ACCELERATION_SMOOTHING))
 	move_and_slide()
 
 func get_movement_vector():
 	var x_movement = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	var y_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
-	
 	return Vector2(x_movement, y_movement)
