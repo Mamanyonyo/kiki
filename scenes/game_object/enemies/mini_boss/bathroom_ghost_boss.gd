2ea00ea2cd415f6ba09d5f86fda7011216071581
Widenyo <n@i.lol>
2024-03-09 04:42:28 -0300

    black

--- scenes/game_object/enemies/mini_boss/bathroom_ghost_boss.gd
+++ scenes/game_object/enemies/mini_boss/bathroom_ghost_boss.gd
@@ -14,6 +14,7 @@ var tilemap: TileMap
 enum{
 	DEFAULT,
 	CHASING_CLOSEST_TOILET,
+	CORRECT_POSITION,
 	SPECIAL_ATTACK
 }
 
@@ -35,6 +36,10 @@ func _process(_delta):
 		CHASING_CLOSEST_TOILET:
 			velocity_component.accelerate_in_direction(objective_direction)
 			velocity_component.move(self)
+		CORRECT_POSITION:
+			global_position = closest_toilet_pos
+			var player_center = get_tree().get_first_node_in_group("Player").get_node("Sprite2D/Middle").global_position
+			sprite.look_at(player_center)
 		SPECIAL_ATTACK:
 			pass
 		_:
@@ -62,29 +67,46 @@ func get_closest_toilet():
 func sort_closest(a, b):
 	return global_position.distance_squared_to(a["position"]) < global_position.distance_squared_to(b["position"])
 
-func _on_event_area_2d_body_entered(body) -> void:
+func _on_event_area_2d_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
 	if !body.is_in_group("player") && (state == CHASING_CLOSEST_TOILET || state == SPECIAL_ATTACK):
-		print("enter")
 		objective_direction = Vector2(0, 0)
 		#visible = false
 		sprite.frame_coords.y = 1
-		tilemap.set_cell(2, selected_toilet_tilemap_pos, 1, Vector2i(10, 2))
-		#var space_state = get_world_2d().direct_space_state
-		#var query = PhysicsRayQueryParameters2D.create(global_position, objective_direction * 100)
-		#var result = space_state.intersect_ray(query)
-		#print(tilemap.local_to_map(tilemap.map_to_local(Vector2i(result.position))))
-		velocity_component.max_speed += 50
+		
+		selected_toilet_tilemap_pos = tilemap.get_coords_for_body_rid(body_rid)
+		change_tile_based_on_tilemap_pos(selected_toilet_tilemap_pos)
+		closest_toilet_pos = tilemap.global_position + Vector2(tilemap.local_to_map(tilemap.map_to_local(selected_toilet_tilemap_pos))) * 16 + Vector2(8, 8)
+		
+		velocity_component.max_speed = 500
 		JumpCDTimer.start()
+		state = CORRECT_POSITION
 
 func _on_timer_timeout() -> void:
-	event_shape.set_deferred("disabled", true)
-	area_cd_timer.start()
+	state = CHASING_CLOSEST_TOILET
 	global_position = closest_toilet_pos
+	change_tile_based_on_tilemap_pos(selected_toilet_tilemap_pos, true)
+	event_shape.set_deferred("disabled", true)
 	var player_center = get_tree().get_first_node_in_group("Player").get_node("Sprite2D/Middle").global_position
-	sprite.look_at(player_center)
 	objective_direction = (player_center - closest_toilet_pos).normalized()
+	sprite.look_at(player_center)
 	visible = true
-
+	area_cd_timer.start()
 
 func _on_area_cd_timeout() -> void:
 	event_shape.set_deferred("disabled", false)
+	
+func change_tile_based_on_tilemap_pos(pos: Vector2i, reset = false):
+	var new_tile: Vector2i
+	if pos.y == 7:
+		new_tile = Vector2i(9, 2)
+	elif pos.x == 13:
+		new_tile = Vector2i(10, 2)
+	elif pos.x == 24:
+		new_tile = Vector2i(11, 2)
+	elif pos.y == 16:
+		new_tile = Vector2i(9, 3)
+	
+	if reset:
+		new_tile.y -= 2
+		
+	tilemap.set_cell(3, pos, 1, new_tile)
