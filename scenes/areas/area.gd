0c1bf848c3bdb3c033bc966061e31d94b7e6eca2
Widenyo <93363813+Widenyo@users.noreply.github.com>
2024-03-14 17:20:22 -0300

    black people be like

--- scenes/areas/area.gd
+++ scenes/areas/area.gd
@@ -3,7 +3,7 @@ class_name Area extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var entities_node = get_node("Entities")
+	var entities_node = get_node("TileMap/Entities")
 	if entities_node != null:
 		var entities_node_array = entities_node.get_children()
 		var main_entities_node = get_tree().get_first_node_in_group("entities_layer")
--- scenes/components/velocity_component.gd
+++ scenes/components/velocity_component.gd
@@ -1,19 +1,17 @@
 class_name VelocityComponent extends Node
 
 @export var acceleration: float = 5
-
 @export var stats_component: StatsComponent
+@export var navigation_agent : NavigationAgent2D
 
 var velocity = Vector2.ZERO
 
 func accelerate_to_player():
 	var owner_node2d = owner as Node2D
 	if owner_node2d == null: return
-	
-	var player = get_tree().get_first_node_in_group("Player") as CharacterBody2D
-	if player == null:
-		return
-	var direction = (player.global_position - owner_node2d.global_position).normalized()
+	##en el futuro, reescribir esto de la forma en que se use el movimiento de siempre para chasear,
+	##pero cuando se este colisionando con el terreno, se haga pathfinding
+	var direction = get_parent().to_local(navigation_agent.get_next_path_position()).normalized()
 	accelerate_in_direction(direction)
 
 func accelerate_in_direction(direction: Vector2):
@@ -24,3 +22,13 @@ func move(character_body: CharacterBody2D):
 	character_body.velocity = velocity
 	character_body.move_and_slide()
 	velocity = character_body.velocity
+
+func make_path():
+	var player = get_tree().get_first_node_in_group("Player") as CharacterBody2D
+	if player == null:
+		return
+	navigation_agent.target_position = player.global_position
+
+
+func _on_timer_timeout() -> void:
+	make_path()
