5a1a3daf497ea16aca7b4b2a3ea9f392a58eefaf
Widenyo <93363813+Widenyo@users.noreply.github.com>
2024-04-01 05:19:19 -0300

    fix

--- autoload/game_events.gd
+++ autoload/game_events.gd
@@ -22,6 +22,7 @@ signal enemy_died(wave_spawned: bool)
 signal wave_enemy_spawned
 signal wave_enemy_limit_reached
 signal quantifiable_stat_changed(incoming_stat_name : String, current, max, percentage)
+signal stat_update
 signal enemy_hit(enemy: BasicEnemy)
 
 func emit_new_spawner(spawner):
@@ -62,5 +63,9 @@ func emit_wave_enemy_limit_reached():
 
 func emit_quantifiable_stat_changed(incoming_stat_name: String, current, max, percentage):
 	quantifiable_stat_changed.emit(incoming_stat_name, current, max, percentage)
+
+func emit_stat_update():
+	stat_update.emit()
+
 func emit_enemy_hit(enemy: BasicEnemy):
 	enemy_hit.emit(enemy)
