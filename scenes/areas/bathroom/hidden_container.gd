8d86adc69d213d92194b48bfcd145eb3caf75fb6
Mamanyonyo <widenyo@national.shitposting.agency>
2024-04-05 19:21:51 -0300

    solvable puzzle

---
+++ scenes/areas/bathroom/hidden_container.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	process_mode = 4
+	hide()
+	GameEvents.toilet_puzzle_complete.connect(on_complete)
+	
+func on_complete():
+	process_mode = 0 # = Mode: Inherit
+	show()
