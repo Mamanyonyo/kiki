1d05297a00178cb21df6baf3d5ebc7ca94d53de1
Mamanyonyo <widenyo@national.shitposting.agency>
2024-04-05 17:35:16 -0300

    equipped item marker

--- scenes/UI/inventory_equipable.gd
+++ scenes/UI/inventory_equipable.gd
@@ -1,13 +1,29 @@
-class_name InventoryItemUI extends MarginContainer
+class_name InventoryItemUI extends PanelContainer
 
 @export var item_name : String
 @export var rect : TextureRect
+
+var active = false
+
+func _ready():
+	GameEvents.item_equip.connect(on_item_equip)
 
 func set_icon() -> void:
 	#if icon_texture != null: icon = icon_texture
 	var image = Image.load_from_file("res://assets/icon/" + item_name + ".png")
 	rect.texture = ImageTexture.create_from_image(image)
+	
+func on_item_equip(id):
+	var new_style_box : StyleBoxFlat
+	if !active && id == item_name:
+		new_style_box = load("res://scenes/UI/inventory_item_equipped_border.tres")
+		active = true
+	else:
+		new_style_box = load("res://scenes/UI/inventory_item_unequipped.tres")
+		active = false
+	add_theme_stylebox_override("panel", new_style_box)
 
 func _on_gui_input(event: InputEvent) -> void:
 	if(event.is_pressed()):
+		#TODO no handlear esto por game events caso de querer darle inventario a alguien mas
 		GameEvents.emit_item_equip(item_name)
--- scenes/game_object/player/WeaponManager.gd
+++ scenes/game_object/player/WeaponManager.gd
@@ -2,17 +2,28 @@ extends Node
 
 @export var player : CharacterBody2D
 @export var current_weapon_controller : WeaponController
+@export var default_controller : WeaponController
 
 func _ready():
+	#TODO no handlear esto por game events caso de querer darle inventario a alguien mas
 	GameEvents.item_equip.connect(on_item_equip)
 
 func _unhandled_input(event: InputEvent) -> void:
 	current_weapon_controller.Inputs()
 
+func equip_item(item: String):
+		var uppercase_name = item[0].to_upper() + item.substr(1,-1)
+		var manager_name = uppercase_name + "Controller"
+		var controller = get_node(manager_name)
+		if manager_name == current_weapon_controller.name && manager_name != default_controller.name:
+			current_weapon_controller.Unequip()
+			current_weapon_controller = default_controller
+			#WARNING hardcodeado
+			GameEvents.emit_item_equip("book")
+		elif controller != null:
+			current_weapon_controller.Unequip()
+			controller.Equip()
+			current_weapon_controller = controller
+
 func on_item_equip(item: String):
-	var manager_name = item[0].to_upper() + item.substr(1,-1) + "Controller"
-	var controller = get_node(manager_name)
-	if controller != null:
-		current_weapon_controller.Unequip()
-		controller.Equip()
-		current_weapon_controller = controller
+	equip_item(item)
