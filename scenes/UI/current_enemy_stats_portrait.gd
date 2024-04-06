0c71fd07da249182353ff32b9a264d9eb22e3841
Widenyo <widenyo@national.shitposting.agency>
2024-04-06 17:19:14 -0300

    show only current sprite on enemy stats ui portion nigs

--- scenes/UI/current_enemy_stats_portrait.gd
+++ scenes/UI/current_enemy_stats_portrait.gd
@@ -5,4 +5,9 @@ func _ready():
 	
 func on_enemy_hit(enemy : BasicEnemy):
 	var sprite = enemy.get_node("Sprite2D") as Sprite2D
-	texture = sprite.texture
+	var atlas : AtlasTexture = AtlasTexture.new()
+	var sprite_size = Vector2(sprite.texture.get_width() / sprite.hframes, sprite.texture.get_height() / sprite.vframes)
+	var current_sprite_pos = Vector2(sprite_size.x * sprite.frame_coords.x, sprite_size.y * sprite.frame_coords.y)
+	atlas.atlas = sprite.texture
+	atlas.region = Rect2(current_sprite_pos.x, current_sprite_pos.y, sprite_size.x, sprite_size.y)
+	texture = atlas
