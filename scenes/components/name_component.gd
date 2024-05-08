5bef2f5507a768c0576f43452351f51718efccd0
Widenyo <widenyo@national.shitposting.agency>
2024-05-07 23:56:07 -0300

    thug hunter the animation

--- scenes/components/name_component.gd
+++ scenes/components/name_component.gd
@@ -13,10 +13,13 @@ func _ready():
 	if types.size() < 1: possible_surnames = names["surname"]["default"]
 	else:
 		for type in types:
-			possible_names += names["name"][type]
-			possible_surnames = names["surname"][type]
+			if names["name"].has(type): possible_names += names["name"][type]
+			else: print("TODO: añadir " + type + " names")
+			if names["surname"].has(type): possible_surnames = names["surname"][type]
+			else: print("TODO: añadir " + type + " surnames")
 	
 	var random_name = possible_names.pick_random()
 	var random_surname = possible_surnames.pick_random()
+	if random_surname == null: random_surname = ""
 	
 	display_name = random_name + " " + random_surname
