ddae184d9e443e47391e11754652a3622ef84174
Mamanyonyo <widenyo@national.shitposting.agency>
2024-05-15 19:48:41 -0300

    bug fix

--- scenes/components/health_component.gd
+++ scenes/components/health_component.gd
@@ -11,14 +11,14 @@ func damage(incoming_damage: float):
 	if final_damage < 0: final_damage = 0
 	stats_component.health -= final_damage
 	changed.emit()
-	if !get_parent().is_in_group("Player"):
+	if !get_parent().is_in_group("Ally"):
 		GameEvents.emit_enemy_hit(get_parent())
 		if final_damage > 0:
 			var floating_text = floating_text_scene.instantiate() as Node2D
 			get_tree().get_first_node_in_group("foreground_layer").add_child(floating_text)
 			floating_text.global_position = get_parent().global_position
 			floating_text.start(str(final_damage))
-	Callable(check_death).call_deferred()
+	check_death()
 	
 func check_death():
 	if stats_component.health <= 0: death()
