91ddfa04e52f93edbed86d36466fcc139f7885f3
Mamanyonyo <widenyo@national.shitposting.agency>
2024-05-22 15:52:02 -0300

    quest fix

--- scenes/game_object/interaction/generic_interactable.gd
+++ scenes/game_object/interaction/generic_interactable.gd
@@ -2,7 +2,10 @@ extends Node
 
 @export var dialogue : DialogueResource
 @export var title : String = "default"
+@export var needed_toilet_for_quest = 3
 
+func _ready():
+	GameEvents.toilet_complete = needed_toilet_for_quest
 
 func on_interact():
 	DialogueManager.show_dialogue_balloon(dialogue, title)
