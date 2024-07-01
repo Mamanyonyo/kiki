396a9fb4ceca68fc3e9d364e25d18461698c4e8f
Mamanyonyo <widenyo@national.shitposting.agency>
2024-07-01 17:19:24 -0300

    enemy melee atack fix

--- scenes/components/player_hurtbox_tracker.gd
+++ scenes/components/player_hurtbox_tracker.gd
@@ -6,10 +6,17 @@ extends Area2D
 
 @export var animation_prefix : String
 
+var in_range = false
+
+func _process(_delta):
+	if in_range && !parent.attacking:
+		parent.attacking = true
+		sprite_manager.animator.play(animation_prefix + "_" + sprite_manager.facing_str)
+		##WARNING no se si esto deberia ir aca
+		sprite_manager.melee_hitbox.damage = stats_component.damage
 
 func _on_area_entered(area: Area2D) -> void:
-	parent.attacking = true
-	sprite_manager.animator.play(animation_prefix + "_" + sprite_manager.facing_str)
-	##WARNING no se si esto deberia ir aca
-	sprite_manager.melee_hitbox.damage = stats_component.damage
-	
+	in_range = true
+
+func _on_area_exited(area: Area2D) -> void:
+	in_range = false
